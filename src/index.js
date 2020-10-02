965971ee1d80f7e2a8e8078f9b7ae60a34c49400
Aditya Toshniwal <aditya.toshniwal14@gmail.com>
2020-10-03 01:33:09 +0530

    Cleanup some unused libs.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Amplify from 'aws-amplify';
-import config from './config.json';
 
-Amplify.configure({
-  Auth:{
-    mandatorySignId:true,
-    region:config.cognito.REGION,
-    userPoolId:config.cognito.USER_POOL_ID,
-    userPoolWebClientId:config.cognito.APP_CLIENT_ID
-  }
-})
 
 ReactDOM.render(
   <React.StrictMode>
