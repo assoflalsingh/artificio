6b958e1eef00a70e9191bda1e94f7fe5c2f58a99
Navdeep Singh <gill.222navdeep@gmail.com>
2022-04-28 04:14:26 +0530

    chore: fix .sh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -8,4 +8,4 @@ echo "${string}"
 # Iterate servers for deploy and pull last commit
 # Careful with the ; https://stackoverflow.com/a/20666248/1057052
 echo "Deploy project on server ${string}"
-ssh ${string} "rm -rf app &&  mkdir app && cd front-end-react && git clone git@gitlab.com:artificio2020/front-end-react.git . && npm install && npm run build && cd build && cp -r * ../../app && cd /var/www/artificio.ai/app && sudo rm -rf * && sudo cp -R /home/ec2-user/app/* . && sudo chown -R nginx:nginx * && sudo systemctl restart nginx.service"
+ssh ${string} "rm -rf app &&  mkdir app && git clone git@gitlab.com:artificio2020/front-end-react.git && cd front-end-react && npm install && npm run build && cd build && cp -r * ../../app && cd /var/www/artificio.ai/app && sudo rm -rf * && sudo cp -R /home/ec2-user/app/* . && sudo chown -R nginx:nginx * && sudo systemctl restart nginx.service"
