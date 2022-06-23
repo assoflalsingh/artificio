f437d9cb025f2214d3b942f2d7c24f6023ab239e
AmanDeep Singh <aman.singh@artificio.ai>
2022-06-24 00:04:05 +0530

    Data View updates on edit, delete, and joins

--- src/routes/DataValidation/DataView/DataViewForm.jsx
+++ src/routes/DataValidation/DataView/DataViewForm.jsx
@@ -40,10 +40,11 @@ export default function DataViewForm({initFormData, ...props}) {
   const [formData, setFormData] = useState(defaults);
   const [formDataErr, setFormDataErr] = useState({});
   const [formSuccess, setFormSuccess] = useState('');
-  const [saving, setSaving] = useState(false);
   const [dgList, setDGList] = useState([]);
   const [labelsList, setLabesList] = useState([]);
-  const {isLoading, apiRequest, error} = useApi();
+  const {isLoading, apiRequest: prequisitesReq, error} = useApi();
+  const {isLoading: dgIsLoading, apiRequest: dgReq, error: dgError} = useApi();
+  const {isLoading: savingDV, apiRequest: saveDVReq, error: dvError} = useApi();
   const classes = useStyles();
 
   const formValidators = {
@@ -54,21 +55,21 @@ export default function DataViewForm({initFormData, ...props}) {
   }
 
   useEffect(()=>{
-    apiRequest({url: URL_MAP.GET_DATAGROUPS}, (response) => {
+    dgReq({url: URL_MAP.GET_DATAGROUPS}, (response) => {
       setDGList(response.datagroups);
     });
-    apiRequest({url: URL_MAP.GET_DATAGROUP_PREQUISITES}, (response) =>{
+    prequisitesReq({url: URL_MAP.GET_DATAGROUP_PREQUISITES}, (response) =>{
       setLabesList(response.labels);
     });
     if(editMode) {
-      console.log(initFormData);
+      initFormData.datagroups = initFormData.datagroups.filter((dg) => dg !== null);
       setFormData({
         ...initFormData,
         date_from: '',
         date_to: '',
       });
     }
-  }, [apiRequest, editMode, initFormData]);
+  }, [dgReq, prequisitesReq, editMode, initFormData]);
 
   const validateField = (name, value) => {
     let errMsg = '';
@@ -119,23 +120,21 @@ export default function DataViewForm({initFormData, ...props}) {
 
     if(isFormValid) {
       let newFormData = {};
+      newFormData = {
+        ...formData,
+        date_from: formData.date_from? `${formData.date_from.getFullYear()}-${appendZero(formData.date_from.getMonth()+1)}-${appendZero(formData.date_from.getDate())}` : '',
+        date_to: formData.date_to? `${formData.date_to.getFullYear()}-${appendZero(formData.date_to.getMonth()+1)}-${appendZero(formData.date_to.getDate())}`: '',
+        datagroups: formData.datagroups.map((label)=>label._id),
+        joins: formData.joins?.length > 0? formData.joins : [] ,
+      }
       if(editMode){
-        newFormData.dataview_desc = formData.dataview_desc;
-      }else{
-        newFormData = {
-          ...formData,
-          date_from: formData.date_from? `${formData.date_from.getFullYear()}-${appendZero(formData.date_from.getMonth()+1)}-${appendZero(formData.date_from.getDate())}` : '',
-          date_to: formData.date_to? `${formData.date_to.getFullYear()}-${appendZero(formData.date_to.getMonth()+1)}-${appendZero(formData.date_to.getDate())}`: '',
-          datagroups: formData.datagroups.map((label)=>label._id),
-          joins: formData.joins?.length > 0? formData.joins : [] ,
-        }
+        delete newFormData._id;
+        delete newFormData.dataview_name;
       }
       console.log("newFormData: ",newFormData);
-      setSaving(true);
-      apiRequest({url: URL_MAP.DATA_VIEW+(editMode? formData._id+'/': ''), params: newFormData, method: editMode?'patch':'post'}, () =>{
+      saveDVReq({url: URL_MAP.DATA_VIEW+(editMode? formData._id+'/': ''), params: newFormData, method: editMode?'patch':'post'}, () =>{
         setFormSuccess(`Data View ${editMode? 'updated':'created'} sucessfully.`);
         if(!editMode) setFormData(defaults);
-        setSaving(false);
       });
     }
   }
@@ -144,7 +143,7 @@ export default function DataViewForm({initFormData, ...props}) {
     <>
     <Box display="flex" style={{marginTop: '1rem'}}>
       <Typography color="primary" variant="h6" gutterBottom>{editMode ? "Edit": "Create"} Data View</Typography>
-      {isLoading && <> <CircularProgress size={24} style={{marginLeft: 15, position: 'relative', top: 4}} /><Typography style={{alignSelf:'center'}}>&nbsp;Loading...</Typography></>}
+      {isLoading && dgIsLoading && <> <CircularProgress size={24} style={{marginLeft: 15, position: 'relative', top: 4}} /><Typography style={{alignSelf:'center'}}>&nbsp;Loading...</Typography></>}
     </Box>
     <Box display="flex">
       <Button
@@ -168,51 +167,51 @@ export default function DataViewForm({initFormData, ...props}) {
       </FormRow>
       <FormRow>
         <FormRowItem>
-          <FormInputText label="Relative Days Data Selection" name='days_before' value={formData.days_before} onChange={onTextChange} disabled={editMode}/>
+          <FormInputText label="Relative Days Data Selection" name='days_before' value={formData.days_before} onChange={onTextChange}/>
         </FormRowItem>
         <FormRowItem>
-          <FormInputText label="To" sideLabel={true} name='days_after' value={formData.days_after} onChange={onTextChange} disabled={editMode}/>
+          <FormInputText label="To" sideLabel={true} name='days_after' value={formData.days_after} onChange={onTextChange}/>
         </FormRowItem>
         <FormRowItem>
           <FormInput label="Relative Date From">
             <CustomField>
-              <DatePicker className={`${classes.dateSelector} MuiInputBase-input MuiOutlinedInput-input`} selected={formData.date_from } placeholderText="Select Date" onChange={date => onTextChange(date, "date_from")} disabled={editMode} />
+              <DatePicker className={`${classes.dateSelector} MuiInputBase-input MuiOutlinedInput-input`} selected={formData.date_from } placeholderText="Select Date" onChange={date => onTextChange(date, "date_from")} />
             </CustomField>
           </FormInput>
         </FormRowItem>
         <FormRowItem>
           <FormInput label="To" sideLabel={true}>
             <CustomField>
-              <DatePicker className={`${classes.dateSelector} MuiInputBase-input MuiOutlinedInput-input`} selected={formData.date_to } placeholderText="Select Date" minDate={formData.date_from} onChange={date => onTextChange(date, "date_to")} disabled={editMode}/>
+              <DatePicker className={`${classes.dateSelector} MuiInputBase-input MuiOutlinedInput-input`} selected={formData.date_to } placeholderText="Select Date" minDate={formData.date_from} onChange={date => onTextChange(date, "date_to")}/>
             </CustomField>
           </FormInput>
         </FormRowItem>
       </FormRow>
       <FormRow>
         <FormRowItem>
-          <FormInputCheck color="primary" label="Include Line Items / Table Data" formData={formData} options={lineItemsTableData} onChange={onTextChange} disabled={editMode} />
+          <FormInputCheck color="primary" label="Include Line Items / Table Data" formData={formData} options={lineItemsTableData} onChange={onTextChange} />
         </FormRowItem>
         <FormRowItem>
-          <FormInputText label="Default no of records (rows)" name="default_rows" value={formData.default_rows} onChange={onTextChange} disabled={editMode} />
+          <FormInputText label="Default no of records (rows)" name="default_rows" value={formData.default_rows} onChange={onTextChange} />
         </FormRowItem>
         <FormRowItem>
-          <FormInputSelect hasSearch multiple label="Data Group" name='datagroups' onChange={(e, value)=>{onTextChange(value, 'datagroups')}} loading={isLoading} value={formData.datagroups} options={dgList} labelKey='name' valueKey='id' disabled={editMode} />
+        <FormInputSelect hasSearch multiple label="Data Group" name='datagroups' onChange={(e, value)=>{onTextChange(value, 'datagroups')}} loading={dgIsLoading} value={formData.datagroups} options={dgList} labelKey='_id' valueKey='_id' />
         </FormRowItem>
         <FormRowItem>
-          <FormInputSelect label="Usage" name='usage' onChange={onTextChange} value={formData.usage} options={['Training','Live']} disabled={editMode} />
+          <FormInputSelect label="Usage" name='usage' onChange={onTextChange} value={formData.usage} options={['Training','Live']} />
         </FormRowItem>
       </FormRow>
-      {!editMode && formData.datagroups.length > 1 && <DataGroupJoins selectedDataGroups={formData.datagroups} labelsList={labelsList} setFormData={setFormData} />}
-      {(error || formSuccess) &&
+      {formData.datagroups.length > 1 && <DataGroupJoins selectedDataGroups={formData.datagroups} labelsList={labelsList} setFormData={setFormData} />}
+      {(error || dgError || dvError || formSuccess) &&
       <FormRow>
         <FormRowItem>
-          {error && <Alert severity="error">{error}</Alert>}
+          {(error || dgError || dvError) && <Alert severity="error">{error || dgError || dvError}</Alert>}
           {formSuccess && <Alert severity="success">{formSuccess}</Alert>}
         </FormRowItem>
       </FormRow>}
       <FormRow>
         <FormRowItem>
-          <Button color="secondary" variant="contained" onClick={onSave} disabled={saving}>{saving ? 'Saving...' : 'Save'}</Button>
+          <Button color="secondary" variant="contained" onClick={onSave} disabled={savingDV}>{savingDV ? 'Saving...' : 'Save'}</Button>
         </FormRowItem>
       </FormRow>
     </Form>
